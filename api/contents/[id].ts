c948ae49dbe946f09040f753434023a0897c2856
Eduardo San Martin Morote <posva13@gmail.com>
2020-07-22 11:21:35 +0200

    try again

--- api/contents/[id].ts
+++ api/contents/[id].ts
@@ -2,5 +2,5 @@ import { NowRequest, NowResponse } from '@vercel/node'
 
 export default async (req: NowRequest, res: NowResponse) => {
   const id = req.query.id as string
-  res.json({ message: 'hello', id })
+  res.json({ message: 'contents', id })
 }
